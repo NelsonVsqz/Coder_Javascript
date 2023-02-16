647b21f1d7ff9e66ce80ef48f3f8b57745c6684a
Nelson Vasquez <nelson10846@gmail.com>
2023-02-16 01:44:03 -0300

    cambiojs_agregomascss

--- creditos.js
+++ creditos.js
@@ -1,33 +1,73 @@
-function calcular(){
-  document.getElementById("tabla").innerHTML="";
-  let capital=Number(document.getElementById("capital").value);
-  let couta=Number(document.getElementById("couta").value);
-  let interes=Number(document.getElementById("interes").value);
-if(capital>0){   
-for(i=1;i<=couta;i++){
- 
- cuotacapital=capital/couta;
- tcuotacapital=cuotacapital.toFixed(2);
- interesmensual=((capital*interes)/100)/couta;
- tinteresmensual=interesmensual.toFixed(2);
- apagar=cuotacapital+interesmensual;
- tapagar=apagar.toFixed(2);
-document.getElementById("tabla").innerHTML=document.getElementById("tabla").innerHTML+
- `<tr>
-    <td> ${i}</td>
-    <td> ${tcuotacapital}</td>
-    <td> ${tinteresmensual}</td>
-    <td> ${tapagar}</td>
-    </tr>`;      }
- sumacapital=capital.toFixed(2);
- sumainteres=interesmensual*couta;
- tsumainteres=sumainteres.toFixed(2);
- sumaapagar=apagar*couta;
- tsumaapagar=sumaapagar.toFixed(2);
- document.getElementById("t1").innerHTML=sumacapital;
- document.getElementById("t2").innerHTML=tsumainteres;
- document.getElementById("t3").innerHTML=tsumaapagar;        
-  }else{
-      alert("Ingrese un numero");
+function limpiarTabla() {
+  document.getElementById("tabla").innerHTML = "";
+}
+
+function obtenerValorInput(idInput) {
+  return Number(document.getElementById(idInput).value);
+}
+
+function mostrarAlerta(mensaje) {
+  alert(mensaje);
+}
+
+function calcularCuotas(capital, cuotas, interes) {
+  let cuotasTabla = "";
+  let cuotaCapital = capital / cuotas;
+  let interesMensual = (capital * interes) / 100 / cuotas;
+  let cuotaTotal = cuotaCapital + interesMensual;
+  
+  for (let i = 1; i <= cuotas; i++) {
+    cuotasTabla += `
+      <tr>
+        <td>${i}</td>
+        <td>${cuotaCapital.toFixed(2)}</td>
+        <td>${interesMensual.toFixed(2)}</td>
+        <td>${cuotaTotal.toFixed(2)}</td>
+      </tr>
+    `;
+  }
+  
+  return {
+    cuotasTabla,
+    sumaCapital: capital.toFixed(2),
+    sumaInteres: (interesMensual * cuotas).toFixed(2),
+    sumaTotal: (cuotaTotal * cuotas).toFixed(2)
+  };
+}
+
+function mostrarResultado(cuotas) {
+  document.getElementById("tabla").innerHTML = cuotas.cuotasTabla;
+  document.getElementById("t1").innerHTML = cuotas.sumaCapital;
+  document.getElementById("t2").innerHTML = cuotas.sumaInteres;
+  document.getElementById("t3").innerHTML = cuotas.sumaTotal;
+}
+
+function calcular() {
+  const capital = obtenerValorInput("capital");
+  const cuotas = obtenerValorInput("couta");
+  const interes = obtenerValorInput("interes");
+
+  if (capital > 0) {
+    const cuotasCalculadas = calcularCuotas(capital, cuotas, interes);
+    mostrarResultado(cuotasCalculadas);
+  } else {
+    mostrarAlerta("Ingrese un número válido");
   }
 }
+
+
+const popup = document.getElementById("popup");
+const inputName = document.getElementById("input-name");
+const submitName = document.getElementById("submit-name");
+const titleName = document.getElementById("title-name");
+
+submitName.addEventListener("click", function() {
+  const name = inputName.value;
+  titleName.innerHTML = "Bienvenido "+name
+  popup.style.display = "none";
+});
+
+popup.style.display = "flex";
+
+
+
